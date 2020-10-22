63c61bde16ceea8fb08c1a23484b39ea1ee2718d
gsanders32 <gray.sanders@gmail.com>
2020-10-22 10:38:47 -0500

    Add project files.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// This is a test API Controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// Gets Values
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Test Info Log");
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Gets Value by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            _logger.LogDebug("Test Debug Log");
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogError("Test Error Log");
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _logger.LogWarn("Test Warning Log");
+        }
+    }
+}
