59448b9ebfb85967c16e8c1c04c7d54440fd23a3
gsanders32 <gray.sanders@gmail.com>
2020-10-22 11:45:42 -0500

    Created Data Classes and DTOs with AutoMapper

--- BookStore-API/Data/ApplicationDbContext.cs
+++ BookStore-API/Data/ApplicationDbContext.cs
@@ -8,6 +8,9 @@ namespace BookStore_API.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        DbSet<Author> Authors { get; set; }
+        DbSet<Book> Books { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
